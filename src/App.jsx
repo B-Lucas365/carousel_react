6c0858225ecd86d50bd54e541f0f91674f85ed24
B-Lucas365 <lucasrenan365@outlook.com>
2022-06-15 14:51:55 -0300

    finalizado funcionalidade carrossel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import logo from './assets/super-shoes.png'
 import './App.scss'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import right from './assets/right.svg'
+import left from './assets/left.svg'
+
 
 export const App = () => {
   const [data, setData] = useState([])
+  const carousel = useRef(null)
 
   useEffect(() => {
     fetch('http://localhost:3000/src/assets/shoes/shoes.json')
     .then(response => response.json())
     .then(setData)
   }, [])
+
+  const handleLeftCick = (e) => {
+    e.preventDefault()
+    carousel.current.scrollLeft -= carousel.current.offsetWidth
+
+  }
+
+  const handleRightClick = (e) => {
+    e.preventDefault()
+    carousel.current.scrollLeft += carousel.current.offsetWidth
+
+  }
 
   if(!data || !data.length) return null
 
@@ -19,22 +35,28 @@ export const App = () => {
         <img src={logo} alt="Supershoes logo" />
       </div>
 
-      <div className="carousel">
-
+      <div className="carousel" ref={carousel}>
+      
         {data.map((item, index) => {
-          const {id, name, price, oldPrice, image} = item;
 
-          <div className='item' key={index}>
-            <div className='image'>
-              <img src={image}  alt={name} />
+          const {id, name, price, oldPrice, image} = item;
+          return(
+            <div className='item' key={index}>
+              <div className='image'>
+                <img src={image}  alt={name} />
+              </div>
+              <div className='info'>
+                <span className='name'>{name}</span>
+                <span className='oldPrice'>{oldPrice}</span>
+                <span className='price'>{price}</span>
+              </div>
             </div>
-            <div className='info'>
-              <span className='name'>{name}</span>
-              <span className='oldPrice'>{oldPrice}</span>
-              <span className='price'>{}</span>
-            </div>
-          </div>
+          )
         })}
+      </div>
+      <div className='buttons'>
+        <button onClick={handleLeftCick}><img src={left} alt="" /></button>
+        <button onClick={handleRightClick}><img src={right} alt="" /></button>
       </div>   
     </div>
   )
